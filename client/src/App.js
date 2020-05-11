5687587b48de7b32d0f83998621ceca8ee5acf4a
Ye Wyn <s3675485@student.rmit.edu.au>
2020-05-11 12:00:22 +1000

    latest

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Router, Route} from 'react-router-dom';
 import MapContainer from './Map'
+import Forecast5 from './OpenWeatherAPI'
 
 function App() {
-  return (
 
-      <MapContainer />
+
+  return (
+  <div>
+    <Forecast5/>
+      </div>
 
   );
 }
