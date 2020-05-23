31d6e28922876b6f281391c5bdefdfafc7c54245
Ye Wyn <s3675485@student.rmit.edu.au>
2020-05-24 06:08:29 +1000

    latest

--- client/src/components/OpenWeatherAPI.js
+++ client/src/components/OpenWeatherAPI.js
@@ -5,20 +5,26 @@ import ForecastView from '../views/ForecastView'
 
 class Forecast5 extends Component {
 
-	cityName = 'Melbourne,au'
 
-	state = {
+	constructor(props){
+	super(props)
+	this.state = {
 		loading: false,
 		forecast: [],
+		cityName: 'Melbourne,au'
+	}
+	this.getForecastData = this.getForecastData.bind(this);
+	this.onTodoChange = this.onTodoChange.bind(this);
 	}
 
 	componentWillMount() {
 		this.getForecastData();
 	}
+	
 
 	async getForecastData() {
 		this.setState({ loading: true });
-		const result = await fetchForecastByCityName(this.cityName);
+		const result = await fetchForecastByCityName(this.state.cityName);
 		this.setState({
 			loading: false,
 			forecast: result.list.map(item => ({
@@ -30,14 +36,31 @@ class Forecast5 extends Component {
 		});
 	}
 
+	onTodoChange(e){
+		console.log(e)
+		this.setState({
+			cityName: e.target.value
+		});
+		console.log(this.cityName)
+    }
+
 	render() {
 		return (
+			<div className="input-container">
+			<h1 className="input-header" id="homeHeader">Enter a Location and Country and Press Enter</h1>
+			<div className="input-controls">
+				<input type="text" placeholder="Melbourne, au" className="form-control" onKeyPress={(e) => {this.onTodoChange(e)}}/>
+				{console.log(this.state.cityName)}
+
+			</div>
 			<ForecastView
-				cityName={this.cityName}
+				cityName={this.state.cityName}
 				forecast={this.state.forecast}
 				loading={this.state.loading}
 				onPressRefresh={() => this.getForecastData()}
 			/>
+					</div>
+
 		);
 	}
 }
