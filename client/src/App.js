5b047e8e4d244aaaaa0473b8bfaee06bdf1728df
s3539822 <s3539822@student.rmit.edu.au>
2020-05-24 23:15:16 +1000

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Header from './Header'
-import Home from './Home'
-import Footer from './Footer'
-import '../css/index.css'
+import Header from './components/Header'
+import Home from './components/Home'
+import Footer from './components/Footer'
+import './css/index.css'
 
 class App extends React.Component {
   render() {
